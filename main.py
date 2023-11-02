f38a602466ec8f7f71eac9c7c686bf60811491d7
Ahmed2650 <111240192+Ahmed2650@users.noreply.github.com>
2023-11-02 16:32:28 +0200

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import math
-Y =int(input("Enter the number of glass for each tray"))
-x = int(input("Enter the number of glass"))
-# the number n of trays need to hold x glass equal
-N = math.ceil(x/Y)
-print(N)
