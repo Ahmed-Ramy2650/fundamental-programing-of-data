a1116693e805fea027d268a4d9e6fca79143ea34
Ahmed Ramy <111240192+Ahmed-Ramy2650@users.noreply.github.com>
2023-11-14 00:48:17 +0200

    Create lab 2.3.py

---
+++ lab2 /lab 2.3.py
@@ -0,0 +1,19 @@
+departuretime = input("Enter the departure time (hh:mm): ")
+departure = departuretime.split(":")
+departurehours = int(departure[0])
+departureminutes = int(departure[1])
+
+arrivaltime = input("Enter the arrival time (hh:mm): ")
+arrival = arrivaltime.split(":")
+arrivalhours = int(arrival[0])
+arrivalminutes = int(arrival[1])
+
+hours = arrivalhours - departurehours
+minutes = arrivalminutes - departureminutes
+
+if minutes < 0:
+    hours -= 1
+    minutes += 60
+
+print("The trip time",hours ,"hr")
+print("The trip time",minutes ,"min")
